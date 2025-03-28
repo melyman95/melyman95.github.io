0224412561f038cbfa2017ac1858df6d23d4fad0
Matthew Lyman <mattlyman12@gmail.com>
2025-03-27 17:11:12 -0700

    initial

---
+++ wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿document.getElementById("fetchButton").addEventListener("click", function () {
+    let query = document.getElementById("fetchInput").value.trim();
+
+    if (query) {
+        // Redirecting to a search page with the query as a URL parameter
+        window.location.href = "/Search?query=" + encodeURIComponent(query);
+    } else {
+        alert("Please enter a search term.");
+    }
+});
